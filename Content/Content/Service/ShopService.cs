ce39a2325bc3200c271ad662b3a51d489c269215
Alex9alexandra <ale02pop@yahoo.com>
2026-03-24 14:33:29 +0200

    Good Main

---
+++ Content/Content/Service/ShopService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Content.Domain;
+using Content.Repository.Interface;
+
+namespace Content.Service;
+
+public class ShopService
+{
+    private readonly IShopRepo _shopRepo;
+
+    public ShopService(IShopRepo shopRepo)
+    {
+        _shopRepo = shopRepo;
+    }
+
+    public IEnumerable<Shop> GetAllAvailableShops()
+    {
+        return _shopRepo.GetAll();
+    }
+
+    public Shop SelectShopById(int id)
+    {
+        if (id < 0)
+        {
+            throw new ArgumentException("ID must not be negative");
+        }
+
+        var shop = _shopRepo.GetById(id);
+
+        if (shop == null)
+        {
+            throw new KeyNotFoundException("Shop with given ID not found");
+        }
+
+        return shop;
+    }
+
+    public void AddShop(Shop shop)
+    {
+
+        var nameExists = _shopRepo.GetAll().Any(s => string.Equals(s.Name, shop.Name));
+
+        if (nameExists)
+        {
+            throw new ArgumentException("Shop name already exists");
+        }
+
+
+        _shopRepo.Add(shop);
+    }
+
+    public void DeleteShop(int id)
+    {
+
+        _shopRepo.Delete(id);
+    }
+
+    // Sorted by name
+    public IEnumerable<Shop> GetShopsSorted()
+    {
+        return _shopRepo.GetAll().OrderBy(shop => shop.Name); 
+    }
+
+    public Shop FindByName(string name)
+    {
+        return _shopRepo.GetAll().FirstOrDefault(shop => string.Equals(shop.Name, name, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void Update(Shop shop)
+    {
+        if (string.IsNullOrWhiteSpace(shop.Name))
+        {
+            throw new ArgumentException("Name field must not be empty");
+        }
+
+        var isDuplicate = _shopRepo.GetAll().Any(s =>
+            s.Id != shop.Id && string.Equals(s.Name, shop.Name));
+
+        if (isDuplicate)
+        {
+            throw new ArgumentException("Shop with given name already exists");
+        }
+
+        _shopRepo.Update(shop);
+    }
+
+}
---
+++ Content/Service/CartService.cs
@@ -0,0 +1,56 @@
+﻿using Content.Domain;
+using Content.Repository.Interface;
+using System.Collections.Generic;
+
+namespace Content.Service
+{
+    public class CartService
+    {
+        private readonly ICartRepo _cartRepo;
+
+        public CartService(ICartRepo cartRepo)
+        {
+            _cartRepo = cartRepo;
+        }
+
+        public IEnumerable<Cart> GetAllCarts()
+        {
+            return _cartRepo.GetAll();
+        }
+
+        public Cart GetCartById(int id)
+        {
+            return _cartRepo.GetById(id);
+        }
+
+        public void AddCart(Cart cart)
+        {
+            _cartRepo.Add(cart);
+        }
+
+        public void DeleteCart(int id)
+        {
+            _cartRepo.Delete(id);
+        }
+
+        public void AddItemToCart(int cartId, CartItem item)
+        {
+            _cartRepo.AddItemToCart(cartId, item);
+        }
+
+        public void RemoveItemFromCart(int cartId, int cartItemId)
+        {
+            _cartRepo.RemoveItemFromCart(cartId, cartItemId);
+        }
+
+        public void UpdateItemQuantity(int cartId, int cartItemId, int quantity)
+        {
+            _cartRepo.UpdateItemQuantity(cartId, cartItemId, quantity);
+        }
+
+        public void ClearCart(int cartId)
+        {
+            _cartRepo.ClearCart(cartId);
+        }
+    }
+}
