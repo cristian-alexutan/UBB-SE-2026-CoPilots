7a840f9e56217889f48d5caa880b9b245402a64d
cristian-alexutan <cristian.alexutan@stud.ubbcluj.ro>
2026-04-21 20:19:37 +0300

    created interface for main service

---
+++ Content/Data/Service/Interface/IMainService.cs
@@ -0,0 +1,18 @@
+﻿using Content.Domain;
+using Content.Service;
+
+namespace Content.Data.Service.Interface
+{
+    public interface IMainService
+    {
+        CartService cartService { get; }
+        ClientService clientService { get; }
+        ManagerService managerService { get; }
+        ReservationService reservationService { get; }
+        IShopItemService ShopItemService { get; }
+        ShopService shopService { get; }
+        ITicketService ticketService { get; }
+
+        IEnumerable<Shop> GetShopsSortedByTickets();
+    }
+}
