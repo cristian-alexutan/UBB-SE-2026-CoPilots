6749bbcb46d1ceb29adffb2db2de02115fdc25e3
emtreila <bmelania214@gmail.com>
2026-04-23 17:27:03 +0300

    added tests for landingviewmodel

---
+++ Content/Tests/ViewModel/LandingViewModelTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Content.Data.Service.Interface;
+using Content.Domain;
+using Content.User;
+using Content.ViewModel;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Tests.ViewModel
+{
+    [TestFixture]
+    public class LandingViewModelTests
+    {
+        private IClientService clientService = null!;
+        private IManagerService managerService = null!;
+        private UserSession session = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            clientService = Substitute.For<IClientService>();
+            managerService = Substitute.For<IManagerService>();
+            session = new UserSession();
+        }
+
+        [Test]
+        public void SetAdminUnsuccessfulTest_AdminIsNull()
+        {
+            managerService.GetAnyManager().Returns((Manager)null!);
+            LandingViewModel vm = new LandingViewModel(clientService, managerService, session);
+
+            vm.SelectAdminCommand.Execute(null);
+
+            Assert.That(vm.IsRoleSelected, Is.False);
+            Assert.That(vm.ErrorMessage, Is.EqualTo("No admin found."));
+        }
+
+        [Test]
+        public void SetAdminSuccessfulTest()
+        {
+            Manager manager = new Manager(7, "John", "john@email.com", "0712345678");
+            managerService.GetAnyManager().Returns(manager);
+            LandingViewModel vm = new LandingViewModel(clientService, managerService, session);
+
+            vm.SelectAdminCommand.Execute(null);
+
+            Assert.That(vm.IsRoleSelected, Is.True);
+            Assert.That(session.IsAdmin, Is.True);
+        }
+
+        [Test]
+        public void SetClientUnsuccessfulTest_ClientIsNull()
+        {
+            clientService.GetAnyClient().Returns((Client)null!);
+            LandingViewModel vm = new LandingViewModel(clientService, managerService, session);
+
+            vm.SelectClientCommand.Execute(null);
+
+            Assert.That(vm.IsRoleSelected, Is.False);
+            Assert.That(vm.ErrorMessage, Is.EqualTo("No client found."));
+        }
+
+        [Test]
+        public void SetClientSuccessfulTest()
+        {
+            Client client = new Client(3, "Jane");
+            clientService.GetAnyClient().Returns(client);
+            LandingViewModel vm = new LandingViewModel(clientService, managerService, session);
+
+            vm.SelectClientCommand.Execute(null);
+
+            Assert.That(vm.IsRoleSelected, Is.True);
+            Assert.That(session.IsAdmin, Is.False);
+        }
+    }
+}
