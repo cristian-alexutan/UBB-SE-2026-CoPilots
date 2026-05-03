70d7a44d5cb60830afec5abb0e55ba421b704f22
emtreila <bmelania214@gmail.com>
2026-05-03 17:48:19 +0300

    mvvn relay command

--- Content/Data/ViewModel/LandingViewModel.cs
+++ Content/Data/ViewModel/LandingViewModel.cs
@@ -1,6 +1,6 @@
 using System.Windows.Input;
 using Content.Data.Service.Interface;
-using Content.Helper;
+using CommunityToolkit.Mvvm.Input;
 using Content.User;
 using Content.ViewModel.Interface;
 
